6d97737da1aa5a5b0d46c0703f456ec177313dba
Manubolgia <obelleiro.liz.manuel@gmail.com>
2023-12-06 10:30:51 +0100

    Added compatibility with 32 as image size

--- guided_diffusion/script_util.py
+++ guided_diffusion/script_util.py
@@ -162,6 +162,8 @@ def create_model(
             channel_mult = (1, 1, 2, 3, 4)
         elif image_size == 64:
             channel_mult = (1, 2, 3, 4)
+        elif image_size == 32:
+            channel_mult = (1, 2, 3, 4)
         elif image_size == 16:
             channel_mult = (1, 2, 3, 4)
         else:
