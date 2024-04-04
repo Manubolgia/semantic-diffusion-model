c6df0367254aa2ad865bbada736196a35ef0de09
Manuel Obelleiro <84449649+Manubolgia@users.noreply.github.com>
2024-04-04 11:09:12 +0200

    Update script_util.py

--- guided_diffusion/script_util.py
+++ guided_diffusion/script_util.py
@@ -158,15 +158,13 @@ def create_model(
             channel_mult = (0.5, 1, 1, 2, 2, 4, 4)
         elif image_size == 256:
             channel_mult = (1, 1, 2, 2, 4, 4)
+        elif image_size == 200:
+            channel_mult = (1, 1, 2, 2, 4, 4)
         elif image_size == 128:
             channel_mult = (1, 1, 2, 3, 4)
         elif image_size == 64:
             channel_mult = (1, 2, 3, 4)
         elif image_size == 32:
-            channel_mult = (1, 2, 3, 4)
-        elif image_size == 24:
-            channel_mult = (1, 2, 3, 4)
-        elif image_size == 16:
             channel_mult = (1, 2, 3, 4)
         else:
             raise ValueError(f"unsupported image size: {image_size}")
